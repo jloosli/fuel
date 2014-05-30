13391abc22f0689713995f9117e315d52922d404
Jared Loosli <jloosli@gmail.com>
2014-05-30 17:29:47 -0600

    Updated Vouchers

--- app/controllers/VoucherController.php
+++ app/controllers/VoucherController.php
@@ -35,7 +35,19 @@ class VoucherController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		$voucher = new Voucher;
+        $voucher->amount = Input::get('amount');
+        $voucher->issued_to = Input::get('issued_to');
+        $voucher->check_id = 23;
+
+        if($voucher->save()) {
+            $result = ['message'=>'success', 'id'=>$voucher->id];
+        } else {
+            $result = ['message'=>'failure'];
+        }
+        return Response::json($result);
+        var_dump($voucher->save());
+        var_dump($voucher->getErrors());
 	}
 
 
--- app/models/Voucher.php
+++ app/models/Voucher.php
@@ -1,10 +1,14 @@
 <?php
 
 class Voucher extends Model {
+    protected $fillable = [
+        'issued_to',
+        'amount'
+    ];
     protected static $rules = [
-        'check_id' => 'required',
+        'check_id'  => 'required',
         'issued_to' => 'required',
-        'amount' => 'required'
+        'amount'    => 'required'
     ];
 
 }
