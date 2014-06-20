c993cf402acc65e14651ff2606f58d03737ae3d6
Jared Loosli <jloosli@gmail.com>
2014-06-20 15:06:29 -0600

    Updated how the system connects to the database

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'fuel',
-            'username'  => 'root',
-            'password'  => 'root',
+            'database'  => $_ENV['DATABASE'],
+            'username'  => $_ENV['USERNAME'],
+            'password'  => $_ENV['PASSWORD'],
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
